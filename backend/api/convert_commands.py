abc810b9346ac745f1d326b48cb7efac28ded717
vladdd0 <vlad123123v@gmail.com>
2023-01-29 02:07:09 +0300

    idk

--- backend/api/convert_commands.py
+++ backend/api/convert_commands.py
@@ -19,18 +19,24 @@ class Json:
         return json.loads(data)
 
 
+async def openFile(file: str, mode: str) -> None:
+    async with open(file=file, mode=mode) as file:
+        return file
+
+
 async def cisco_to_eltex(data: str) -> str:
+    # TODO: Доделать
     eltex_code: list = {}
+    cisco_code: list = {}
     try:
-        async with open(file=CISCO_SYNTAX_FILE, mode="r") as file:
-            commands: list = Json.load(data=file)
+        cisco_file = await openFile(file=CISCO_SYNTAX_FILE, mode="r") 
+        commands: list = Json.load(data=cisco_file)
         cisco_code: dict = data.split("\n") 
         for i in range(len(cisco_code)):
             cisco_code[i]
-
+        return eltex_code
     except Exception:
         return logger.error("{excp}", excp=Exception)
-    return
 
 
 async def eltex_to_cisco(data: str) -> str:
