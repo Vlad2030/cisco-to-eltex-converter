e752f6c463c46658dc1777042b36526ba4a889b0
vladdd0 <vlad123123v@gmail.com>
2023-01-29 00:44:24 +0300

    template

---
+++ main.py
@@ -0,0 +1,6 @@
+import app
+import frontend.telegram
+import backend.api
+
+if __name__ == "__main__":
+    pass
---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import platform
+
+
+def installLib() -> None:
+    system = platform.system()
+    try:
+        if "Linux" in system:
+            return os.system("pip3 install -r requirements.txt")
+        elif "Windows" or "Darwin" in system:
+            return os.system("pip install -r requirements.txt")
+    except Exception:
+        return print(Exception)
+
+
+if __name__ == "__main__":
+    installLib()
